e9e4e975d0f84bc1610ad4c1740eaa1da4a2d9ff
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-08-19 16:40:37 +0200

    new final version

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,18 @@
 "use client";
 
 import { EmailThread } from "@/components/EmailThread";
-import { ProvideApiKeyDialog } from "@/components/ProvideApiKeyDialog";
 import { EmailsProvider } from "@/lib/hooks/use-emails";
 import { CopilotKit } from "@copilotkit/react-core";
-import "@copilotkit/react-ui/styles.css";
-import { useSearchParams } from "next/navigation";
+import "@copilotkit/react-textarea/styles.css";
 
 export default function Home() {
-  const searchParams = useSearchParams();
-  const publicApiKey = searchParams.get("publicApiKey");
+  const publicApiKey = process.env.NEXT_PUBLIC_COPILOT_CLOUD_PUBLIC_API_KEY;
 
   return (
     <>
       <div className="h-screen flex flex-col">
-        <div className="flex-[0_0_auto]">
-          <ProvideApiKeyDialog />
-        </div>
         <div className="flex-1">
-          <CopilotKit publicApiKey={(publicApiKey as string) || "undefined"}>
+          <CopilotKit publicApiKey={(publicApiKey as string)}>
             <EmailsProvider>
               <EmailThread />
             </EmailsProvider>
