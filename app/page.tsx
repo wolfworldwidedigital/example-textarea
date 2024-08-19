2cb7e6c0da9aae3f6f6d210ca1aeb4d619810511
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-08-19 16:58:23 +0200

    minimal starting point

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,15 @@
 
 import { EmailThread } from "@/components/EmailThread";
 import { EmailsProvider } from "@/lib/hooks/use-emails";
-import { CopilotKit } from "@copilotkit/react-core";
-import "@copilotkit/react-textarea/styles.css";
 
 export default function Home() {
-  const publicApiKey = process.env.NEXT_PUBLIC_COPILOT_CLOUD_PUBLIC_API_KEY;
-
   return (
     <>
       <div className="h-screen flex flex-col">
         <div className="flex-1">
-          <CopilotKit publicApiKey={(publicApiKey as string)}>
-            <EmailsProvider>
-              <EmailThread />
-            </EmailsProvider>
-          </CopilotKit>
+          <EmailsProvider>
+            <EmailThread />
+          </EmailsProvider>
         </div>
       </div>
     </>
