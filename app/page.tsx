2efd630094374c2f9a172e8c6948aa2f360d4123
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-08-19 17:25:27 +0200

    update base

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,10 @@ import { EmailsProvider } from "@/lib/hooks/use-emails";
 
 export default function Home() {
   return (
-    <>
-      <div className="h-screen flex flex-col">
-        <div className="flex-1">
-          <EmailsProvider>
-            <EmailThread />
-          </EmailsProvider>
-        </div>
-      </div>
-    </>
+    <div className="h-screen">
+      <EmailsProvider>
+        <EmailThread />
+      </EmailsProvider>
+    </div>
   );
 }
